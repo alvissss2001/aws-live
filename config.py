fcd622adc6fe09283c2990f636e8130f74d0099d
alvissss2001 <103267287+alvissss2001@users.noreply.github.com>
2022-04-08 22:58:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cllpc0j6ebro.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "ngzheyu-employee"
+customregion = "us-east-1"
